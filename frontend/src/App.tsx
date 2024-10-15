b53181f58940794c2c97ed2079b89bdd52f7f8eb
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-10-15 21:02:08 +0200

    build fetch to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { IProduct } from './interfaces/productInterface';
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [initialArticlesPage, setInitialArticlesPage] = useState(100)
+  const [getInitialProduts, setGetInitialProduts] = useState<IProduct[]>();
+  
+  const getProductsRandom = async () => {
+       const response = await fetch(`http://localhost:4050/products`);
+       const data: IProduct[] = await response.json();
+        console.log(data)
+       setGetInitialProduts(data);
+
+  }
+
+  useEffect(() => {
+    
+    getProductsRandom();
+    
+  }, [])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+
+      <section className="header">
+        <img src="public/viafirma-logo.svg"/>
+        <div className="header__name-buttons">
+          <h1>Listado de productos</h1>
+          <div className="header__name-buttons--buttons">
+              <button>Ordenar por Skus</button>
+              <button>Ordenar por Precio Asc</button>
+              <button>Ordenar por Precio Desc</button>
+          </div>
+        </div>
+      </section>
+
+      <section className="content">
+        <div className="content__fields">
+          <p>sku</p>
+          <p>Name of product</p>
+          <p>Price</p>
+        </div>
+        <div className="content__products">
+        {/* Aqui se hace el map y es lo que se va a repetir a muerte */}
+          <div className="content__products--fields">
+            <p>sku</p>
+            <p>Name of product</p>
+            <p>Price</p>
+          </div>
+        </div>
+        <div className="content__paginator-buttons">
+          <p>Texto enseñando X de 1000</p>
+          <button>Next 100</button>
+          <button>Prev 100</button>
+        </div>
+
+      </section>
+
+    </main>
   )
 }
 
---
+++ frontend/src/interfaces/productInterface.ts
@@ -0,0 +1,5 @@
+export interface IProduct {
+  sku: number
+  name: string
+  price: number
+}
