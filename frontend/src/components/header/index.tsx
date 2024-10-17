78346c409040157e8f87af5e6b947b261010287d
Daniel Sanguino <153718537+danisanguino@users.noreply.github.com>
2024-10-17 22:05:42 +0200

    finaly app version

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import "./header.css"
+
+export const Header = () => {
+  return (
+
+    <section className="header">
+
+    <img height={34} src="/viafirma-logo.svg"/>
+    <h1>Prueba técnica</h1>
+    
+    </section>
+
+  )
+}
